a1d3dbde65b5c5497267bf2c7e25ef70ddfad9e5
oiviana <vianalucas80@gmail.com>
2025-10-21 22:05:38 -0300

    fix: cors await

--- src/index.ts
+++ src/index.ts
@@ -27,23 +27,30 @@ server.addHook('onRequest', (request, reply, done) => {
   done();
 });
 
+const allowedOrigins = [
+  'https://next-auth-products-frontend-production.up.railway.app',
+  'https://next-auth-products-frontend-cog7.vercel.app',
+  'http://localhost:3000',
+];
+
 const start = async () => {
   try {
     await server.register(cors, {
-      origin: [
-        'https://next-auth-products-frontend-production.up.railway.app',
-        'https://next-auth-products-frontend-cog7.vercel.app',
-        'http://localhost:3000',
-      ],
+      origin: (origin, cb) => {
+        if (!origin) return cb(null, true); 
+        if (allowedOrigins.includes(origin)) {
+          cb(null, true);
+        } else {
+          cb(new Error("Not allowed by CORS"), false);
+        }
+      },
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true,
-      preflightContinue: false,
     });
 
-    server.register(authentication);
+    await server.register(authentication);
     await app(server);
-
 
     await server.listen({ port: serverPort, host: '0.0.0.0' });
     console.info(`🚀 Server running on port ${serverPort}`);
