d0c85bdef56159281736ddc6794645bfbdfdfc28
oiviana <vianalucas80@gmail.com>
2025-10-22 00:01:19 -0300

    remove unused middleware

--- src/middlewares/verifyJwt.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyReply, FastifyRequest } from "fastify";
-
-export async function verifyJwt(req: FastifyRequest, reply: FastifyReply) {
-  try {
-    await req.jwtVerify();
-  } catch (err) {
-    return reply.status(401).send({ message: "Token inválido ou ausente" });
-  }
-}
